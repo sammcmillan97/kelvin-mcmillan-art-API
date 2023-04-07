6ab6489d4cb989f0188e66cca9657c3d8a634a7b
Sam <smc397@uclive.ac.nz>
2023-04-07 19:52:38 +1200

    Added User pydantic model as a template and endpoint for getting all users.

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 # main.py
+from typing import List
+from uuid import uuid4
 
 from fastapi import FastAPI
+from models import User, Gender, Role
 
 app = FastAPI()
+
+db: List[User] = [
+    User(id=uuid4(),
+         first_name="Sam",
+         last_name="McMillan",
+         gender=Gender.male,
+         roles=[Role.admin]
+         ),
+    User(id=uuid4(),
+         first_name="Angus",
+         last_name="McMillan",
+         gender=Gender.male,
+         roles=[Role.admin]
+         ),
+]
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db
---
+++ models.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import Optional, List
+from uuid import UUID, uuid4
+from enum import Enum
+
+
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+
+
+class Role(str, Enum):
+    admin = "admin"
+    user = "user"
+
+
+class User(BaseModel):
+    id: Optional[UUID] = uuid4()
+    first_name: str
+    last_name: str
+    gender: Gender
+    roles: List[Role]
