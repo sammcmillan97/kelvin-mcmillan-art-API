8bcaf42e4f7982c646a8bac60b33d07d93edbacd
Gussssy <angus.mcmillan.94@gmail.com>
2023-11-30 22:17:23 +1300

    add swagger for giclees and portfolio page

--- database/pydantic_schemas.py
+++ database/pydantic_schemas.py
@@ -2,26 +2,53 @@ from pydantic import BaseModel
 from typing import List
 
 # why...? do we really need inheritance here. 
-class Painting():
+class Painting(BaseModel):
     title: str
+    type: str
+    dimensions: str
+    sold: bool
+    giclee: bool
+    image_url: str
+    price: str
+
     class Config:
         orm_mode = True
 
 
-class GetPaintings(BaseModel):
-    title:  str
-    class Config:
-        orm_mode = True
-
-
-class GetGiclees(BaseModel):
-    title: str
-    class Config:
-        orm_mode = True
-
-
-class GetPortFolioPage(BaseModel):
+class GetPaintingsResponse(BaseModel):
     paintings: List[Painting]
+    class Config:
+        orm_mode = True
 
+
+class GetOriginalsResponse(BaseModel):
+    title: str
+    class Config:
+        orm_mode = True
+
+
+class GetPortfolioPageResponse(BaseModel):
+    paintings: List[Painting]
+    class Config:
+        orm_mode = True
+
+
+class GicleeOption(BaseModel):
+    dimensions: str
+    price: str
+    class Config:
+        orm_mode = True
+
+
+
+class Giclee(BaseModel):
+    painting: Painting
+    options: List[GicleeOption]
+    class Config:
+        orm_mode = True
+
+
+class GetGicleesResponse(BaseModel):
+    paintings: List[Giclee]
     class Config:
         orm_mode = True
--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import sqlite3
 #from routers import painting_routers
+from database.pydantic_schemas import GetPortfolioPageResponse, GetOriginalsResponse, GetGicleesResponse
 
 
+import sys
+import os
 
-app = FastAPI()
+project_root = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, project_root)
+
+
+app = FastAPI(arbitrary_types_allowed=True)
+
+@app.get("/paintings/{page}", response_model=GetPortfolioPageResponse)
+async def get_paintings_for_page(page: str):
+    return {"Id": 1, "Title": "A Title", "Type": "Watercolour", "Dimensions": "760x480mm", "Page": page }
+
+@app.get("/paintings/originals", response_model=GetOriginalsResponse)
+async def get_orginals(page: str):
+    return {"Id": 1, "Title": "A Title", "Type": "Watercolour", "Dimensions": "760x480mm", "Page": page }
+
+
+@app.get("/paintings/giclees", response_model=GetGicleesResponse)
+async def get_giclees(page: str):
+    return {"Id": 1, "Title": "A Title", "Type": "Watercolour", "Dimensions": "760x480mm", "Page": page }
 
 
 # app = FastAPI()
@@ -20,9 +40,19 @@ app = FastAPI()
 #         ('Lindis Pass', 200)
 # """)
 
-
 # @app.get("/")
 # async def root():
 #     res = cur.execute("SELECT * FROM painting")
 #     return_painting = res.fetchone()
 #     return {"Painting": return_painting}
+
+# Dependency to print information
+def print_dependency():
+    print("This is being executed")
+    print("Executing print_dependency")
+    print(sys.path)
+    return True
+
+@app.get("/")
+async def read_root(print_info: bool = Depends(print_dependency)):
+    return {print(sys.path)}
